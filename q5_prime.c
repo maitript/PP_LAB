8f045cc22a975d37cdb1c41b88cad1d522b1288d
Maitri P Tadas <93444567+maitript@users.noreply.github.com>
2023-12-18 20:51:23 +0530

    Update q5_prime.c

--- q5_prime.c
+++ q5_prime.c
@@ -1,37 +1,39 @@
-//primes in 1-n
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<omp.h>
 
 int isPrime(int n){
-  if(n == 0 || n == 1){
+  if(n < 2)
     return 0;
-  }
-  for(int i = 2; i <= n/2; i++){
-    if(n%i == 0){
+  for(int i = 2; i <= n/2; i++)
+    if(n%i == 0)
       return 0;
-    }
-  }
   return 1;
 }
 
 int main(){
+	int n;
+	printf("Enter n value");
+	scanf("%d",&n);	
 
-  int x = 1,n = 50;
-  int primes[100];
+	//parallel
+	double start = omp_get_wtime();
+	#pragma omp parallel for
+	for(int i = 1; i <= n;i++)
+		if(isPrime(i))
+			printf("process : %d , %d \n",omp_get_thread_num(),i);
+	double end = omp_get_wtime();
+	double para = end-start;
 
-  double start = omp_get_wtime();
-  #pragma omp parallel for
-  for(int i = 1; i <= n;i++){
-    if(isPrime(i)){
-      int t = omp_get_thread_num();
-      printf("process : %d , %d \n",t,i);
-    }
-  }
-  double end = omp_get_wtime();
-  printf("The time taken is %lf ",end - start);
-  // for(int i = 0; i < n; i++){
-    //If needed print numbers here from primes[i]
-  // }
+	//serial
+	start = omp_get_wtime();
+	for(int i = 1; i <= n;i++)
+		if(isPrime(i))
+			printf("process : %d , %d \n",omp_get_thread_num(),i);
+	end = omp_get_wtime();
+	double seri = end -start;
+
+	//print times
+	printf("The time taken for parallel is %lf\n\n ",para);
+	printf("The time taken for serial is %lf ",seri);
 }
