cd06a5bc6e206c8cfcdee754c5ef8dc4e4c35101
Maitri P Tadas <93444567+maitript@users.noreply.github.com>
2023-12-18 22:58:56 +0530

    Update q2_deadlock.c

--- mpi/q2_deadlock.c
+++ mpi/q2_deadlock.c
@@ -3,9 +3,11 @@
 void main(){
     int rank,size,root=0,dest=1,source=0,tag1=1,tag2=2;
     int out =77,in;
+    
     MPI_Init(NULL,NULL);
-    MPI_Status stat;
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+    MPI_Status stat;
+    
     //no deadlock
     if(rank==0){
         dest = 1;
@@ -19,6 +21,7 @@ void main(){
         MPI_Recv(&in,1,MPI_INT,source,tag2,MPI_COMM_WORLD,&stat);//change tags here for deadlock
         MPI_Send(&out,1,MPI_INT,dest,tag1,MPI_COMM_WORLD); //once tags changes no output
    }
+    
     printf("rank %d msg %d with tag %d from task %d\n",rank,in,stat.MPI_TAG,stat.MPI_SOURCE);
     MPI_Finalize();
 }
